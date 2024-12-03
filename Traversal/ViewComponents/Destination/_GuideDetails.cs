0e0e7985133ad73693670f0b339a193d22ed0334
mgurkankaya <https://github.com/mgurkankaya>
2024-12-03 22:55:29 +0300

    78- Tasarımsal düzenlemeler

---
+++ Traversal/ViewComponents/Destination/_GuideDetails.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.Destination
+{
+    public class _GuideDetails:ViewComponent
+    {
+        private readonly IGuideService _guideService;
+
+        public _GuideDetails(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var value = _guideService.TGetById(6);
+            return View(value);
+        }
+
+    }
+}
